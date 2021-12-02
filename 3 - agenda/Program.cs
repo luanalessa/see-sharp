e77aa793db62f4466050ec6a5e2f73cc110669cc
isobrrj <58219535+isobrrj@users.noreply.github.com>
2021-12-02 19:35:24 -0300

    Update Program.cs
    Main recebendo entrada de dados.  
    no arquivo Person.cs eu removi a herança para não travar. 
    
    uma verificação foi implementada no Name. se o cliente não informar o nome, um erro vai aparecer no console

--- 3 - agenda/Program.cs
+++ 3 - agenda/Program.cs
@@ -7,49 +7,30 @@ namespace _4___register
     {
         static void Main(string[] args)
         {
-            // Document doc = new Document("111.111.111-11");    
-            // Document doc1 = new Document("11111111111");    
-            // Document doc2 = new Document("123.456.789-09");    
-            // Document doc3 = new Document("12345678909");    
-    
-            // Document doc4 = new Document("11111111111111");    
-            // Document doc5 = new Document("11.444.777/0001-61");    
-            // Document doc6 = new Document("59360748000148");            
-       
-            Person person = new Person(
-                "12345678909",
-                "Luana Lessa Sena",
-                "lessa@gmail.com",
-                "Rua 9");  
             
-            Agenda agenda = new Agenda();
-            Console.WriteLine(person.Name);
+            Person person = new Person();
+            string nameUser, enderecoUser, emailUser;
+            int documentUser;
 
-            // person.createContact(
-            //     "88999999999",
-            //     "Joao Pedro Saraceni",
-            //     "joao@gmail.com",
-            //     "Rua 7"
-            // );
+            Console.WriteLine("Informe o seu nome: ");
+            nameUser = Console.ReadLine();
 
-            // person.createContact(
-            //     "88999999993",
-            //     "Luana Lessa Sena",
-            //     "lessa@gmail.com",
-            //     "Rua 8"
-            // );
+            Console.WriteLine("Informe o seu endereço: ");
+            enderecoUser = Console.ReadLine(); ;
 
-            // person.updateContact(
-            //     "88999999993",
-            //     "Luana Lessa Sena",
-            //     "lessa@gmail.com",
-            //     "Rua 13"
-            // );
+            Console.WriteLine("Informe o seu e-mail: ");
+            emailUser = Console.ReadLine();
 
-            // person.readContact();
+            Console.WriteLine("Informe o seu documento: ");
+            documentUser = int.Parse(Console.ReadLine());
 
-            // person.deleteContact("88999999999");
-            // person.readContact();
+
+            Console.WriteLine("===============================");
+
+            Console.WriteLine(person.Name = nameUser);
+            Console.WriteLine(person.Address = enderecoUser);
+            Console.WriteLine(person.Email = emailUser);
+            Console.WriteLine(person.Document = documentUser);
 
 
 
